0dc3182b12a85450c7ba102e5b560a46166d3cfc
Yujun Wu <wyj0912@gmail.com>
2013-12-21 15:50:03 -0800

    not using rmagick for now

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-require 'rmagick'
-include Magick
+#require 'rmagick'
+#include Magick
 
 class PhotosController < ApplicationController
 	def home
-    @image_list = ImageList.new("#{images_root}/test/image1.jpg", "#{images_root}/test/image2.jpg")
-    @image = @image_list.append(false)
+    #@image_list = ImageList.new("#{images_root}/test/image1.jpg", "#{images_root}/test/image2.jpg")
+    #@image = @image_list.append(false)
     #@image.display()
     #@image.write("#{images_root}/test/composed_image.jpg")
 	end
